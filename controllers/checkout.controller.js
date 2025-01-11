0317508ac475b3111e4bb06fc3ed7e5bf3d1bd67
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-01-11 12:25:04 +0200

    Checkout Cart Get Controller

---
+++ controllers/checkout.controller.js
@@ -0,0 +1,8 @@
+import { handleResponse } from "../util/handleResponse.js";
+
+export const checkoutCart = async (req, res, next) => {
+  return handleResponse(res, {
+    success: true,
+    renderView: "checkout",
+  });
+};
