eea635141a53ee6b3a16d9f0adb77bf9aff3940f
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-02-04 19:06:32 +0200

    Update Auth Routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 import {
   authLogout,
+  getForgetPassword,
   getLogin,
+  getResetPassword,
   getSignup,
+  postForget,
   postLogin,
   postSignup,
+  resetPassword,
 } from "../controllers/auth.controller.js";
 import multer from "multer";
 import { isGuest } from "../middleware/auth.middleware.js";
@@ -16,6 +20,8 @@ router
   .get(isGuest, getSignup)
   .post(multer().any(), postSignup);
 router.route("/login").get(isGuest, getLogin).post(multer().any(), postLogin);
+router.route("/forget-password").get(getForgetPassword).post(multer().any(), postForget);
+router.route("/reset-password/:userId/:refreshToken").get(getResetPassword).post(multer().any(), resetPassword);
 router.route("/logout").all(authLogout);
 
 export const authRouter = router;
